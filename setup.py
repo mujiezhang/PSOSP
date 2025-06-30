ef37190155644a33f03530d99de053444de2eb0f
mujiezhang <67539352+mujiezhang@users.noreply.github.com>
2025-06-30 15:10:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="psosp",
-    version="1.1.1",  
+    version="1.1.2",  
     py_modules=['psosp'], 
     author="Mujie Zhang", 
     author_email="3224413991@qq.com", 
