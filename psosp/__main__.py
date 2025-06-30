ea8f959b01d61d42f26529f371a85b5e4f9c1b22
mujiezhang <67539352+mujiezhang@users.noreply.github.com>
2025-06-30 21:10:25 +0800

    Add files via upload

---
+++ psosp/__main__.py
@@ -0,0 +1,9 @@
+"""
+This is the main entry point for the command line tool.
+It's designed to be executed when a user runs `python -m psosp`.
+"""
+
+from psosp import main
+
+if __name__ == '__main__':
+    main() 
